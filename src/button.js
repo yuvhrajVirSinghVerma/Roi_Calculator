e0498f8d5bce69c392e83c977f586803496e965e
yuvhrajVirSinghVerma <99727445+yuvhrajVirSinghVerma@users.noreply.github.com>
2022-02-17 23:35:56 +0530

    Delete button.js

--- src/button.js
+++
@@ -1,8 +0,0 @@
-export function createBtn(n,className,...args){
-    for(let i=0;i<n;i++){
-         let btn =document.createElement('button')
-         btn.innerText=args[i]
-         btn.classList.add(className)
-         document.body.append(btn)
-    }
-}
